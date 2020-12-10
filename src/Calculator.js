adbfc29e9678d048a621e01caaa63d35f44bcea6
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-10 08:47:31 -0600

    [Caluclator.js] Subtract, mulitply, divide

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,6 +7,7 @@ class Calculator extends Component {
             result: 0,
             num1: 0,
             num2: 0,
+            operator: "+",
         }
     }
 
@@ -14,20 +15,41 @@ class Calculator extends Component {
         this.setState({[num]: parseInt(e.target.value)});
     }
 
+    operator = (e) => {
+        let type = e.target.value;
+        this.setState({operator: type});
+    }
+
     getSum = (e) => {
+        if(this.state.operator === "+") {
+            let sum = this.state.num1 + this.state.num2;
+            this.setState({ result: sum })
+        } else if (this.state.operator === "-") {
+            let sum = this.state.num1 - this.state.num2;
+            this.setState({ result: sum })
+        } else if (this.state.operator === "/") {
+            let sum = this.state.num1 / this.state.num2;
+            this.setState({ result: sum })
+        } else if (this.state.operator === "*") {
+            let sum = this.state.num1 * this.state.num2;
+            this.setState({ result: sum })
+        }
         e.preventDefault();
-        let sum = this.state.num1 + this.state.num2
-        this.setState({ result: sum })
     }
 
     render() {
         return (
             <div className="container">
-                <h1>Add with React!</h1>
+                <h1>React Calculator!</h1>
 
                 <div className="add">
                     <input type="number" placeholder="EnterYour first number" value={this.state.num1} onChange={(e) => this.setNum(e, 'num1')}/>
-                    <span>+</span>
+                    <select onChange={this.operator}>
+                        <option>+</option>
+                        <option>-</option>
+                        <option>/</option>
+                        <option>*</option>
+                    </select>
                     <input type="number" placeholder="Enter your second number" value={this.setState.num2} onChange={(e) => this.setNum(e, 'num2')}/>
                     <button onClick={this.getSum}>=</button>
                     <h3>{this.state.result}</h3>
